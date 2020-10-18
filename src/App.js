0bff4ba1dcb734bd4be0c9bb6aa3d2c6e2d859ae
Henry S. Sjøen <hsjo@equinor.com>
2020-10-18 11:31:35 +0200

    Add instructions on how to get calendar on mobile

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-
-function useWindowSize() {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    const [windowSize, setWindowSize] = useState({
-        width: undefined,
-        height: undefined,
-    });
-
-    useEffect(() => {
-        // Handler to call on window resize
-        function handleResize() {
-            // Set window width/height to state
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        }
-
-        // Add event listener
-        window.addEventListener("resize", handleResize);
-
-        // Call handler right away so state gets updated with initial window size
-        handleResize();
-
-        // Remove event listener on cleanup
-        return () => window.removeEventListener("resize", handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
-
-    return windowSize;
-}
+import {useWindowSize} from "./utils/UseWindowSize";
 
 function App() {
     const size = useWindowSize();
     const mobile = size.width <= 960;
 
     return (
-        <div style={{minWidth:400}}>
+        <div style={{minWidth: 400}}>
             <h1 style={{margin: 24, fontFamily: 'Helvetica'}}>Team 5</h1>
             <div style={{
                 flexDirection: 'row',
@@ -47,13 +17,40 @@ function App() {
                 margin: 24
             }}>
                 <div style={{margin: 12}}>
+                    <h2>Hvordan få T5 kalenderen på din mobil?</h2>
+                    <p>Dersom du er på iPhone så må du først legge til Google-kontoen din i instillinger:
+                    </p>
+                    <ul>
+                        <li>Select Passwords & Accounts and then Add Account and then Google.</li>
+                    </ul>
+                    <p>Så kan du legge til kalenderen og velge at den skal synkroniseres til din mobil.</p>
+                    <ol>
+                        <li><a
+                            href="https://calendar.google.com/calendar/u/0/r?cid=7bjdc11fbtikpgicg0vaufqm7k@group.calendar.google.com">
+                            <button style={{padding: 8, margin: 12}}>Legg
+                                til kalenderen i Google Calendar
+                            </button>
+                        </a>
+                        </li>
+                        <li><a href="https://calendar.google.com/calendar/u/0/syncselect">
+                            <button style={{padding: 8, margin: 12}}>Kryss av "T5 - Lovsang" for
+                                synkronisering
+                            </button>
+                        </a></li>
+                    </ol>
+                    Dersom du har noe trøbbel:
+                    <a href="https://m.me/sjoenh">
+                        <button style={{padding: 8, margin: 12}}>{`Send Henry 🤓 en melding`}</button>
+                    </a>
+                </div>
+                <div style={{margin: 12}}>
                     <h2>Kalender</h2>
-                    {mobile ?
-                        <iframe
+                    {mobile
+                        ? <iframe
                             title='Kalender'
                             src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FOslo&amp;src=N2JqZGMxMWZidGlrcGdpY2cwdmF1ZnFtN2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%2344a703&amp;showTitle=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;showNav=1&amp;mode=AGENDA"
-                            height="400" frameBorder="0" scrolling="no"/> :
-                        <iframe
+                            height="400" frameBorder="0" scrolling="no"/>
+                        : <iframe
                             title='Kalender'
                             src="https://calendar.google.com/calendar/embed?src=7bjdc11fbtikpgicg0vaufqm7k%40group.calendar.google.com&ctz=Europe%2FOslo"
                             style={{width: 800, height: 600}} frameBorder="0" scrolling="no"/>
@@ -63,7 +60,7 @@ function App() {
                     <h2>Spotify spilleliste</h2>
                     <iframe
                         title='Spotify Spilleliste'
-                        src="https://open.spotify.com/embed/playlist/2gKC1cadlh0QnGWyx4fKlK"
+                        src="https://open.spotify.com/embed/playlist/1EiZh1zjDKnz1BhF2mjCDy"
                         width="300" height="380" frameBorder="0"
                         allowTransparency="true" allow="encrypted-media"
                     />
@@ -71,11 +68,20 @@ function App() {
                 <div style={{margin: 12}}>
                     <h2>Lenker</h2>
                     <ul>
-                        <li><a href="https://www.messenger.com/t/1966802903393312">T5 Chat</a></li>
-                        <li><a href="https://www.messenger.com/t/3232098980208574">T5 Info</a></li>
-                        <li><a href="https://www.dropbox.com/sh/5a3gmzjoyoauc8q/AACU-RyZ2OpjAVKR2Nr2cX6Ra?dl=0">Noter og
-                            slikt
-                            -> Dropbox</a></li>
+                        <li>
+                            <a href="https://www.messenger.com/t/1966802903393312">
+                                <button style={{padding: 8, margin: 12}}>
+                                    T5 Chat
+                                </button>
+                            </a></li>
+                        <li><a href="https://www.messenger.com/t/3232098980208574">
+                            <button style={{padding: 8, margin: 12}}>
+                                T5 Info
+                            </button>
+                        </a></li>
+                        <li><a href="https://www.dropbox.com/sh/5a3gmzjoyoauc8q/AACU-RyZ2OpjAVKR2Nr2cX6Ra?dl=0">
+                            <button style={{padding: 8, margin: 12}}>Noter og slikt -> Dropbox</button>
+                        </a></li>
                     </ul>
                 </div>
             </div>
